61307af60faceb6acd848e5cdca2658a8e81d9ef
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-13 17:54:52 +0300

    This commit creates an index,register,login,logout,profile view function

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html',)
+
+def register_user(request):
+    return render(request, 'auth/register.html',)
+
+def login_user(request):
+    return render(request, 'auth/login.html',)
+
+def log_out(request):
+    logout(request)
+    return redirect('login')
+
+def profile(request):
+    return render(request, 'profile.html',)
