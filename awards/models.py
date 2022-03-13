f35af2d7d50f6d7390e024fb5814fc9c9429a49a
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-13 22:16:52 +0300

    This commit creates the profile model

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=1000, blank=True)
+    role = models.CharField(max_length=150)
+    github = models.CharField(max_length=200, blank=True)
+    profile_pic=models.ImageField(upload_to='images/')
+
+
