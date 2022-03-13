08f4e86f4c18c0764dc32fc3ec0f69e0b189cd30
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-13 23:25:08 +0300

    This commit configures our urls.py to register the MEDIA_ROOT route to serve uploaded images on the development server

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,3 +11,5 @@ urlpatterns = [
     path('accounts/logout/', views.logout_user, name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
