435602c20d0a7aee6c551229fe4f3bfdd6cb1e2c
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-13 17:33:22 +0300

    This commit imports the url function from the django.conf.urls and the apps views module and i create a list named urlpatterns which will be a list of url instances for the app

---
+++ awards/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('auth/register/', views.register_user, name='register'),
+    path('auth/login', views.login_user, name='login'),
+    path('accounts/logout', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
+]
