9e0b1ae13dc53b1c76f8b1779587c8b2c64f26cc
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-14 02:01:15 +0300

    This commit creates new URLConfs to handle requests  for the project and profile lists api profiles

--- awards/urls.py
+++ awards/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('auth/login/', views.login_user, name='login'),
     path('accounts/logout/', views.logout_user, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('project/<project_id>', views.project, name='project'),
+    path('api/profiles/', views.ProfileList.as_view()),
+    path('api/projects/', views.ProjectsList.as_view())
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
