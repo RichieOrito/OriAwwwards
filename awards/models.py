8aec8ab14dd44eea17be153af48f60a6854e16c4
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-13 22:44:59 +0300

    This commit creates the projects model

--- awards/models.py
+++ awards/models.py
@@ -10,4 +10,12 @@ class Profile(models.Model):
     github = models.CharField(max_length=200, blank=True)
     profile_pic=models.ImageField(upload_to='images/')
 
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    screenshot = models.ImageField('images')
+    posted_at = models.DateField(auto_now_add=True)
+    repository_link = models.CharField(max_length=200, blank=True)
+    live_link = models.CharField(max_length=200, blank=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects')
 
